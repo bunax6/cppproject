4c94bf441f9c029a1d623072745494e5c16aa328
Bunax <bunax6@gmail.com>
2020-03-10 19:06:05 +0530

    two file added

--- imain.cpp
+++ imain.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
 using namespace std;
+#include "sample.h"
 
 int main()
 {
+	sample s;
 	cout<<"first line"<<endl;
 	cout<<"hello"<<endl;
 }
---
+++ sample.h
@@ -0,0 +1,5 @@
+class sample
+{
+	public:
+	sample();
+};
